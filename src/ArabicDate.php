00221ad5fc3168b196bc1c7c0d71903282f0bbf5
Mohamed Yousef <modi401@hotmail.com>
2021-01-23 14:00:15 +0200

    convert gregorian to hijri .. done

--- src/ArabicDate.php
+++ src/ArabicDate.php
@@ -94,11 +94,75 @@ class ArabicDate
     /**
      * Convert the current date into hijri
      * 
+     * NOTE : this function based on "Kuwaiti Algorithm" and this code
+     *        original source can be found at :
+     *        https://www.al-habib.info/islamic-calendar/hijricalendartext.htm
+     * 
      * @return array
      */
     private function gregorianToHijri()
-    {
+    {        
+        $day   = (float) date('d');
+        $month = (float) date('m');
+        $year  = (float) date('Y');
         
+        $y = $year;
+        $m = $month;
+        if ($m < 3) {
+            $y -= 1;
+            $m += 12;
+        }
+
+        $a = floor($y / 100.0);
+        $b = (2 - $a) + floor($a / 4.0);
+        $jd = floor(365.25 * ($y + 4716)) + floor(30.6001 * ($m + 1)) + $day + $b - 1524;
+        
+        if ($jd > 2299160) {
+            $a = floor(($jd - 1867216.25) / 36524.25);
+            $b = 1 + $a - floor($a / 4.0);
+        }
+
+        $bb = $jd + $b + 1524;
+        $cc = floor(($bb - 122.1) / 365.25);
+        $dd = floor(365.25 * $cc);
+        $ee = floor(($bb - $dd) / 30.6001);
+        $day = ($bb - $dd) - floor(30.6001 * $ee);
+        $month = $ee - 1;
+        
+        if ($ee > 13) {
+            $cc += 1;
+            $month = $ee - 13;
+        }
+
+        $year = $cc - 4716;
+        $iyear = 10631.0 / 30.0;
+        $epochastro = 1948084;
+        $epochcivil = 1948085;
+        
+        $shift1 = 8.01 / 60.0;
+        
+        $z = $jd - $epochastro;
+        $cyc = floor($z / 10631.0);
+        $z = $z - (10631 * $cyc);
+        
+        $j = floor(($z - $shift1) / $iyear);
+        $iy = (30 * $cyc) + $j;
+        $z = $z - floor(($j * $iyear) + $shift1);
+        
+        $im = floor(($z + 28.5001) / 29.5);
+        if ($im == 13) $im = 12;
+        
+        $id = $z - floor((29.5001 * $im) - 29);
+
+        $myRes = [];
+        $myRes[0] = $day;
+        $myRes[1] = $month;
+        $myRes[2] = $year;
+        $myRes[4] = $id;
+        $myRes[5] = $im;
+        $myRes[6] = $iy;
+
+        return $myRes;
     }
 
     /**
@@ -130,7 +194,7 @@ class ArabicDate
         $nowGregorian = date($this->format);
 
         /** Convert Date **/
-        $nowHijri = '';
+        $nowHijri = $this->gregorianToHijri();
 
         /** Baset on the calendar & language return the output **/
         if ($this->language === 'arabic') {
