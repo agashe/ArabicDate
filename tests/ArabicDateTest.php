ab282f9632669f63176cac8598e1e1a34affc021
Mohamed Yousef <modi401@hotmail.com>
2021-03-27 16:42:08 +0200

    working on test cases

--- tests/ArabicDateTest.php
+++ tests/ArabicDateTest.php
@@ -2,13 +2,66 @@
 
 include "../src/ArabicDate.php";
 
+use PHPUnit\Framework\TestCase;
 use ArabicDate\ArabicDate;
 
-date_default_timezone_set('Africa/Cairo');
+class ArabicDateTest extends TestCase
+{
+    public function testSetFormat()
+    {
+        $tries = 0;
+        $invalidValues = [50, 100.50, false, true, null, [], new stdClass()];
+        $validValues = ['d/m/y', 'H:i:s', 'D - M - Y', 'j/n/Y H:i:s A', 'test'];
 
-try {
-    $foo = new ArabicDate('D d/m/Y h:i:s A', 'english', 'hijri');
-    print $foo->get() . PHP_EOL;
-} catch (\Throwable $th) {
-    print $th->getMessage() . PHP_EOL;
+        foreach (array_merge($invalidValues, $validValues) as $value) {
+            try {
+                $date = new ArabicDate();
+                $date->setFormat($value);
+            }
+            catch (Exception $error) {
+                $tries += 1;
+            }
+        }
+
+        $this->assertEquals($tries, count($invalidValues));
+    }
+    
+    public function testSetLanguage()
+    {
+        $tries = 0;
+        $validValues = ['arabic', 'english'];
+        $invalidValues = [50, 100.50, false, true, null, [], new stdClass()];
+
+        foreach ($invalidValues as $value) {
+            try {
+                $date = new ArabicDate();
+                $date->setLanguage($value);
+            }
+            catch (Exception $error) {
+                $tries += 1;
+            }
+        }
+
+        $this->assertEquals($tries, count($invalidValues));
+    }
+    
+    public function testSetCalendar()
+    {
+        $tries = 0;
+        $validValues = ['hijri', 'gregorian'];
+        $invalidValues = [50, 100.50, false, true, null, [], new stdClass()];
+
+        foreach ($invalidValues as $value) {
+            try {
+                $date = new ArabicDate();
+                $date->setLanguage($value);
+            }
+            catch (Exception $error) {
+                $tries += 1;
+            }
+        }
+
+        $this->assertEquals($tries, count($invalidValues));
+    }
 }
+
